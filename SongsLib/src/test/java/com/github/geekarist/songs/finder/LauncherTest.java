1da54828d906353ebad7af345077c5a10819f953
geekarist <christophe.pele@gmail.com>
2012-07-22 18:56:09 +0200

    Update launcher test (now failing) for using youtube authentication

--- SongsLib/src/test/java/com/github/geekarist/songs/finder/LauncherTest.java
+++ SongsLib/src/test/java/com/github/geekarist/songs/finder/LauncherTest.java
@@ -24,18 +24,32 @@ public class LauncherTest {
 		expectNext(scanner, "rock");
 		expectPrint(out, "Enter number of results: ");
 		expectNextInt(scanner, 3);
-		
+
 		expectPrintf(out, "Found %d songs\n", 3);
 		expectPrintln(out, new Song("Living Colour", "Cult of Personality"));
-		expectPrintln(out, new Song ("Alice Cooper", "I'm Eighteen"));
+		expectPrintln(out, new Song("Alice Cooper", "I'm Eighteen"));
 		expectPrintln(out, new Song("The Used", "I Caught Fire"));
-		
+
+		expectPrintf(out, "Please go to %s in your web browser and enter this code: %s",
+				"http://www.google.com/device", "user_code");
+		expectPrintfAnyStringArg(out, "Authorization OK, token = [%s]");
+
 		EasyMock.replay(scanner, out);
 
 		Launcher launcher = new Launcher(scanner, out, confPath);
 		launcher.launch();
 
 		EasyMock.verify(scanner, out);
+	}
+
+	private void expectPrintfAnyStringArg(PrintStream out, String string) {
+		out.printf(EasyMock.eq(string), EasyMock.isA(String.class));
+		EasyMock.expectLastCall().andReturn(out);
+	}
+
+	private void expectPrintf(PrintStream out, String fmt, String arg1, String arg2) {
+		out.printf(EasyMock.eq(fmt), EasyMock.eq(arg1), EasyMock.eq(arg2));
+		EasyMock.expectLastCall().andReturn(out);
 	}
 
 	private void expectPrintln(PrintStream out, Song song) {
